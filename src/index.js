2d0e32ebbb0045826d760425d0c0029f5afc40da
Kutik Gómez <144839359+Kutik-Gomez@users.noreply.github.com>
2024-08-19 22:26:51 -0500

    cambio de puerto
    7777

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3535;
+const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
   console.log(`Servicio iniciado en el puerto ${PORT}`);
 });
